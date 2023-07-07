21d8373f6d1f9f66eb241ba6940e0c367ccc67db
NERY DAVID MENDEZ <4216528@estudiantes.mined.edu.sv>
2023-07-07 10:34:54 -0600

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace App_Cambio;
+
+public partial class MainPage : ContentPage
+
+{
+
+	public MainPage()
+
+	{
+
+		InitializeComponent();
+
+	}
+
+    /// <summary>
+    /// <CreateDate>07/07/2023</CreateDate>
+    /// <Company>SandBox</Company>
+    /// <Lastmodificationdate>07/07/2023</Lastmodificationdate>
+    /// <lastmodificationdescript>Se imprime el cambio correspondiente en un label</lastmodificationdescript>
+    /// <lastmodificationautor>Nery</lastmodificationautor>
+    /// </summary>
+
+    // Programamos el evento click del button para que este sea capaz de generar lo que le estamos solicitando //
+    private void OnCounterClicked(object sender, EventArgs e)
+
+    // Declaramos las variables C,D Y V. y les asiganamos sus valores correspondientes //
+
+	{
+
+        int C = Convert.ToInt32(Costo.Text);
+
+        int D = Convert.ToInt32(Dinero.Text);
+
+        // Imprimimos el vuelto que se dara en el label Cambio en su propiedad text //
+
+        int V = D - C; Cambio.Text = V.ToString();
+
+    }
+
+}
+
